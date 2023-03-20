6e10efde72937d520acf7db93e73b3cc061e11d7
that-jamal <varg.jamil@gmail.com>
2023-03-20 11:05:41 +0100

    add

--- main.js
+++ main.js
@@ -2,33 +2,38 @@ let room = document.body
 let roomName = document.getElementById("roomName")
 let nextId = 0
 
-function mapAdd() {
-    back.style.backgroundImage = (frame[nextId].view)
+function Add() {
+    room.style.backgroundImage = frame[nextId].room
+    console.log(nextId)
     //------------------------------------------------------ 
 }
 
 document.body.addEventListener("keypress", function (event) {
     if (event.key == "w") {
         nextId = (frame[nextId].mapId)
-        mapAdd()
+        Add()
     }
     if (event.key == "a") {
         nextId = (frame[nextId].leftmapId)
-        mapAdd()
+        Add()
     }
     if (event.key == "s") {
         nextId = (frame[nextId].backmapId)
-        mapAdd()
+        Add()
     }
     if (event.key == "d") {
         nextId = (frame[nextId].rightmapId)
-        mapAdd()
+        Add()
     }
 })
 
 const frame = [
     {//0
         room: "url('https://cdn.discordapp.com/attachments/678621053026828309/1073160741273731082/image.png')",
-        mapId: 0, rightmapId: 1, leftmapId: 0, backmapId: 0, roomId: "room"
+        mapId: 1, rightmapId: 1, leftmapId: 1, backmapId: 1, roomId: "room"
+    },
+    {//0
+        room: "url('https://cdn.discordapp.com/attachments/678621053026828309/1073160741273731082/image.png')",
+        mapId: 0, rightmapId: 0, leftmapId: 0, backmapId: 0, roomId: "room"
     },
 ]
